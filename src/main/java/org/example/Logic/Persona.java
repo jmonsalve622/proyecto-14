668c7aa349a2f3a1f5194c146f7cd2281d2c05bb
Joaquín Monsalve <joaquinmonsalve0206@gmail.com>
2025-06-21 16:26:43 -0400

    Creación de la clase Persona

---
+++ src/main/java/org/example/Logic/Persona.java
@@ -0,0 +1,32 @@
+package org.example.Logic;
+
+public class Persona {
+    private String nombre;
+    private String correo;
+    private int id;
+
+    public Persona(String nombre, String correo, int id) {
+        this.nombre = nombre;
+        this.correo = correo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+}
